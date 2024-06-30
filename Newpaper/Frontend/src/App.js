865a0672702557e5e6816c0fd1b63b6abcea8a8c
nguyenthephung <136545799+nguyenthephung@users.noreply.github.com>
2024-06-30 09:40:02 +0700

    .

--- Newpaper/Frontend/src/App.js
+++ Newpaper/Frontend/src/App.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import HomePage from "./Admin/Components/Home/HomePage";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Login from "./Admin/Components/Login/Login";
-import Register from "./Admin/Components/Register/Register";
-import NavBar from "./Admin/Components/NavBar/NavBar";
-import CustomerHome from "./Customer/CustomerHome/CustomerHome";
-import { useState } from "react";
-
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <div className="App"> 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={ <Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/test" element={<CustomerHome/>} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-=======
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import PageContent from "./Admin/Components/PageContent";
@@ -48,4 +20,3 @@ function App() {
 }
 
 export default App;
->>>>>>> Phung
