48721891084cd0c02b6b93f3a11bd51b78aecc8a
Phuc114 <128774565+Phuc114@users.noreply.github.com>
2024-08-30 00:00:16 +0700

    Tag_model.js

---
+++ Newpaper/Backend/models/Tag_model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const tagsSchema= new Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    category :{type:String},
+  }, { timestamps: true });
+  
+  module.exports = mongoose.model('tags', tagsSchema);
+  
